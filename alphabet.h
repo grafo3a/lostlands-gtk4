c5c9fa245f448aa9bf5ce7c6a612ab50babec0a9
ApasaJB <jb.apasa@gmail.com>
2024-11-13 11:27:16 +0100

    feat: add alphabet.h

---
+++ alphabet.h
@@ -0,0 +1,91 @@
+#ifndef ALPHABET_H
+#define ALPHABET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+char getPreviousLetter(char lettre) {
+
+    lettre = toupper(lettre);
+    char prevLetter;
+
+    switch (lettre) {
+        case 'A': prevLetter = '-'; break;
+        case 'B': prevLetter = 'A'; break;
+        case 'C': prevLetter = 'B'; break;
+        case 'D': prevLetter = 'C'; break;
+        case 'E': prevLetter = 'D'; break;
+        case 'F': prevLetter = 'E'; break;
+        case 'G': prevLetter = 'F'; break;
+        case 'H': prevLetter = 'G'; break;
+        case 'I': prevLetter = 'H'; break;
+        case 'J': prevLetter = 'I'; break;
+        case 'K': prevLetter = 'J'; break;
+        case 'L': prevLetter = 'K'; break;
+        case 'M': prevLetter = 'L'; break;
+        case 'N': prevLetter = 'M'; break;
+        case 'O': prevLetter = 'N'; break;
+        case 'P': prevLetter = 'O'; break;
+        case 'Q': prevLetter = 'P'; break;
+        case 'R': prevLetter = 'Q'; break;
+        case 'S': prevLetter = 'R'; break;
+        case 'T': prevLetter = 'S'; break;
+        case 'U': prevLetter = 'T'; break;
+        case 'V': prevLetter = 'U'; break;
+        case 'W': prevLetter = 'V'; break;
+        case 'X': prevLetter = 'W'; break;
+        case 'Y': prevLetter = 'X'; break;
+        case 'Z': prevLetter = 'Y'; break;
+        case '-': prevLetter = 'Z'; break;
+
+    default:
+        prevLetter = '-'; break;
+    }
+
+    return prevLetter;
+}
+
+char getNextLetter(char lettre) {
+
+    lettre = toupper(lettre);
+    char nextLetter;
+
+    switch (lettre) {
+        case 'A': nextLetter = 'B'; break;
+        case 'B': nextLetter = 'C'; break;
+        case 'C': nextLetter = 'D'; break;
+        case 'D': nextLetter = 'E'; break;
+        case 'E': nextLetter = 'F'; break;
+        case 'F': nextLetter = 'G'; break;
+        case 'G': nextLetter = 'H'; break;
+        case 'H': nextLetter = 'I'; break;
+        case 'I': nextLetter = 'J'; break;
+        case 'J': nextLetter = 'K'; break;
+        case 'K': nextLetter = 'L'; break;
+        case 'L': nextLetter = 'M'; break;
+        case 'M': nextLetter = 'N'; break;
+        case 'N': nextLetter = 'O'; break;
+        case 'O': nextLetter = 'P'; break;
+        case 'P': nextLetter = 'Q'; break;
+        case 'Q': nextLetter = 'R'; break;
+        case 'R': nextLetter = 'S'; break;
+        case 'S': nextLetter = 'T'; break;
+        case 'T': nextLetter = 'U'; break;
+        case 'U': nextLetter = 'V'; break;
+        case 'V': nextLetter = 'W'; break;
+        case 'W': nextLetter = 'X'; break;
+        case 'X': nextLetter = 'Y'; break;
+        case 'Y': nextLetter = 'Z'; break;
+        case 'Z': nextLetter = '-'; break;
+        case '-': nextLetter = 'A'; break;
+
+    default:
+        nextLetter = '-'; break;
+    }
+
+    return nextLetter;
+}
+
+#endif // ALPHABET_H
